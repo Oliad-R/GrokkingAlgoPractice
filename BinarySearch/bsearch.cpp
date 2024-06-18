fa0fa87a736232f95ed078301eeef9e0dc65dca5
Oliad Rundassa <runol@utschools.ca>
2024-06-17 20:38:27 -0400

    fixed binary search.

--- BinarySearch/bsearch.cpp
+++ BinarySearch/bsearch.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 
 int rand_array[13] = {1,2,3,5,8,9,10,20,30,103,204,1002,39992};
 
@@ -7,7 +8,7 @@ int binary_search(int target, int arr[], int arr_size){
   int high = arr_size-1;
   int mid = arr_size / 2;
 
-  while (low<high){
+  while (low<=high){
     mid = (low + high) / 2;
 
     if(arr[mid]==target){
@@ -22,11 +23,13 @@ int binary_search(int target, int arr[], int arr_size){
 }
 
 int main(){
-  int target = 1002;
+  int target;
+  std::cout << "What would you like to find?: ";
+  std::cin >> target;
   int size = sizeof(rand_array)/sizeof(rand_array[0]);
   int index = binary_search(target, rand_array, size);
   if(index==-1){
-    printf("The target was not found in the array.");
+    printf("The target was not found in the array.\n");
   } else {
     printf("The target %d was found at index %d.\n", target, index);
   }
